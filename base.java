23d04f797f4d8dbee6e12830a74c4906459c276f
kornelio <kornelioreyna@gmail.com>
2020-07-22 19:48:12 -0500

    adding comments to base file.java

--- base.java
+++ base.java
@@ -16,7 +16,8 @@ public class base {
 		File fs=new File(f,"ApiDemos-debug.apk");
 		
 		DesiredCapabilities cap=new DesiredCapabilities();
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "PixelAndroid9");
+		//New comment added in line X
+		cap1.setCapability(MobileCapabilityType.DEVICE_NAME, "PixelAndroid9");
 		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
 		
 		cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
